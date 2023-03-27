1993cfe1853bf96f87ca2c0f78a51e1cecfe061d
Kevinobote <omoshk49@gmail.com>
2023-03-27 04:18:27 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a pinter to an int as param
+ * and updating value it points to 98
+ * @n: input
+ * Returns: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
