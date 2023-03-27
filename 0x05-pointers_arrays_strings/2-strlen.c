9b9530163a1192285f5adb6df3456e56e680cfd6
Kevinobote <omoshk49@gmail.com>
2023-03-27 09:24:13 -0700

    2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
 /**
  * _strlen - Returns the length of a str
  * @str: The string to get the lnght
- * Returns: The length of @str.
+ * Return: Always 0 (The length of @str.)
  */
 
 size_t _strlen(const char *str)
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
@@ -21,3 +24,5 @@ char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
 int _putchar(char c);
+
+#endif /*MAIN_H*/
